e908c228f6213d05d689ea0e531dfc8e0dd83d40
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-03-26 10:48:48 -0600

    new styling for contact page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,20 +1,47 @@
 import React from "react";
 import './Contact.css';
+import gitLogo from '../asset/github.png';
+import linkedLogo from '../asset/linkedin.png';
 
 const Contact = () => {
-    return (
-        <div className="contact-page">
-            <div className="contact-container">
-                <h1>Get in Touch</h1>
-                <p>Feel free to reach out for inquiries or collaborations. I look forward to connecting with you!</p>
-                <div className="my-info-container">
-                    <p><strong>Email:</strong> <a href="mailto:zmnimstfa@gmail.com">zmnimstfa@gmail.com</a></p>
-                    <p><strong>GitHub:</strong> <a href="https://github.com/mstfazmni" target="_blank" rel="noopener noreferrer">https://github.com/mstfazmni</a></p>
-                    <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/mo-zamani" target="_blank" rel="noopener noreferrer">www.linkedin.com/in/mo-zamani</a></p>
+  return (
+      <div className="contact-container">
+        <div className="contact-wrapper">
+          <section className="contact-left-sec">
+              <h1 className="h1-contact">GET IN</h1>
+              <span className="span-contact">touch</span>
+              <div className="left-sec-inside-container">
+
+                <div className="input-container">
+                    <input type="text" placeholder="Your name"/>
+                    <input type="email" placeholder="Your email address"/>
                 </div>
-            </div>
-        </div>
-    )
+                
+                <div className="action-container">
+                    <textarea placeholder="Write to me..."></textarea>
+                    <button className="submit-btn">Submit</button>
+                </div>
+                
+              </div>
+
+              <div className="logos-email-container">
+                  <div className="logos-div">
+                      <a href="https://github.com/mstfazmni" target="_blank" rel="noopener noreferrer">
+                        <img src={gitLogo} alt="GitHub" className="git-logo" />
+                      </a>
+                      <a href="https://www.linkedin.com/in/mo-zamani" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedLogo} alt="LinkedIn" className="linked-logo" />
+                      </a>
+                  </div>
+                  <div className="email-div">
+                    <p>You can also email me</p>
+                    <a href="mailto:zmnimstfa@gmail.com">zmnimstfa@gmail.com</a>
+                  </div>
+              </div>
+          </section>   
+        </div>  
+      </div>
+  );
 }
 
 export default Contact;
