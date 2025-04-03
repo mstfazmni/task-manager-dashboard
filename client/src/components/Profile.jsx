3145f44c3ec4f847cc6124a5451a8d70b3eadf5a
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-04-03 09:42:48 -0600

    Profile back to dashboard button

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from "react";
 import './Profile.css';
 import userImg from '../asset/user-img.png';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Profile = () => {
     //retrive user info
@@ -12,6 +15,12 @@ const Profile = () => {
         }
     },[]);
 
+    //Navigate button to main page
+    const navigate = useNavigate();
+    const handleNavigateDashboardClick = () => {
+        navigate('/dashboard');
+    }
+
     return(
         <div className="profile-container">
             {/* section 1 */}
@@ -20,6 +29,7 @@ const Profile = () => {
                 <img src={userImg} className="user-img" alt="userimage"></img>
                 <p className="user-name-div">{userInfo ? `${userInfo.firstName} ${userInfo.lastName}` : 'Loading...'}</p>
                 <p className="user-job">Software Developer</p>
+                <button className="navigateBtn" onClick={handleNavigateDashboardClick}>Back To Dashboard</button>
 
             </section>
             {/* section 2 */}
